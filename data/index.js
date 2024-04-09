2b324e1ec44020c1b55f1beb6ae702b95a616021
unknown <geovanheewofc123@gmail.com>
2024-04-09 13:41:11 -0300

    versa com data

---
+++ data/index.js
@@ -0,0 +1,37 @@
+[
+    {
+        id: 1,
+        title="Tomar café e me preparar para o dia",
+        description="Acordar às 09:00 e preparar-me para o dia ate as 12:00",
+        imageUrl="https://cdn.worldvectorlogo.com/logos/c--4.svg",
+        percent="12.5",
+    },
+    {
+        id: 2,
+        title="Ir para a escola",
+        description="Ficar na escola das 12:00 até as 19:00.",
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/0/00/AssemblyScript_logo_2020.svg",
+        percent="29.2",
+    },
+    {
+        id: 3,
+        title="Estudar programação",
+        description="Estudar programação das 19:00 até as 21:00.",
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/c/cf/Lua-Logo.svg",
+        percent="8.3",
+    },
+    {
+        id: 4,
+        title="Lazer",
+        description="Tempo livre das 21:00 às 00:00.",
+        imageUrl="https://www.svgrepo.com/show/303206/javascript-logo.svg",
+        percent="12.5"
+    },
+    {
+        id: 5,
+        title="Dormir à meia-noite",
+        description="Ir para a cama às 00:00 para descansar até as 09:00",
+        imageUrl="https://cdn.worldvectorlogo.com/logos/c.svg",
+        percent="37.5"
+    },
+]
